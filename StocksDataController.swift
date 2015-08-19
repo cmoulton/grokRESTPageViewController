056d0d1ba1cd4f8194fed7ddb72f4d908608571e
Christina Moulton <christina@teakmobile.com>
2015-08-19 16:43:20 -0300

    Converted to use dataController (rough MVVM)

---
+++ StocksDataController.swift
@@ -0,0 +1,43 @@
+//
+//  StocksDataController.swift
+//  
+//
+//  Created by Christina Moulton on 2015-08-19.
+//
+//
+
+import Foundation
+import UIKit
+
+class StocksDataController {
+  var dataObjects = NSMutableArray() // NSMutableArray because Swift arrays don't have objectAtIndex
+  
+  func loadStockQuoteItems(completionHandler: (NSError?) -> Void) {
+    let symbols = ["AAPL", "GOOG", "YHOO"]
+    StockQuoteItem.getFeedItems(symbols, completionHandler:{ (items, error) in
+      if error != nil
+      {
+        completionHandler(error)
+        return
+      }
+      if let stocks = items {
+        for stock in stocks { // because we're getting back a Swift array but it's easier to do the PageController in an NSMutableArray
+          self.dataObjects.addObject(stock)
+        }
+      }
+      // success
+      completionHandler(nil)
+    })
+  }
+  
+  func indexOfStock(stock: StockQuoteItem) -> Int {
+    return dataObjects.indexOfObject(stock)
+  }
+  
+  func stockAtIndex(index: Int) -> StockQuoteItem? {
+    if (index < 0 || index > dataObjects.count - 1) {
+      return nil
+    }
+    return dataObjects[index] as? StockQuoteItem
+  }
+}
--- grokPageViewController/GrokPageViewController.swift
+++ grokPageViewController/GrokPageViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class GrokPageViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
   
-  var dataObjects = NSMutableArray() // NSMutableArray because Swift arrays don't have objectAtIndex
+  let dataController = StocksDataController()
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -24,65 +24,46 @@ class GrokPageViewController: UIPageViewController, UIPageViewControllerDataSour
     let placeholderVC = storyboard.instantiateViewControllerWithIdentifier("PlaceHolderViewController") as! UIViewController
     self.setViewControllers([placeholderVC], direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
     
-    loadStockQuoteItems()
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  func loadStockQuoteItems() {
-    let symbols = ["AAPL", "GOOG", "YHOO"]
-    StockQuoteItem.getFeedItems(symbols, completionHandler:{ (items, error) in
+    dataController.loadStockQuoteItems{ (error) in
       if error != nil
       {
         var alert = UIAlertController(title: "Error", message: "Could not load stock quotes \(error?.localizedDescription)", preferredStyle: UIAlertControllerStyle.Alert)
         alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
         self.presentViewController(alert, animated: true, completion: nil)
-      }
-      if let stocks = items {
-        for stock in stocks { // because we're getting back a Swift array but it's easier to do the PageController in an NSMutableArray
-          self.dataObjects.addObject(stock)
+      } else {
+        // set first view controller to display
+        if let firstVC = self.viewControllerAtIndex(0) {
+          let viewControllers = [firstVC]
+          self.setViewControllers(viewControllers, direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
         }
       }
-
-      // set first view controller to display
-      if let firstVC = self.viewControllerAtIndex(0)
-      {
-        let viewControllers = [firstVC]
-        self.setViewControllers(viewControllers, direction: UIPageViewControllerNavigationDirection.Forward, animated: true, completion: nil)
-      }
-    })
+    }
   }
   
   // MARK: UIPageViewControllerDataSource & UIPageViewControllerDelegate
   func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
-    if let currentPageViewController = viewController as? SinglePageViewController, currentStock:AnyObject = currentPageViewController.stock {
-      let currentIndex = dataObjects.indexOfObject(currentStock)
+    if let currentPageViewController = viewController as? SinglePageViewController, currentStock:StockQuoteItem = currentPageViewController.stock {
+      let currentIndex = dataController.indexOfStock(currentStock)
       return viewControllerAtIndex(currentIndex - 1)
     }
     return nil
   }
   
   func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
-    if let currentPageViewController = viewController as? SinglePageViewController, currentStock:AnyObject = currentPageViewController.stock {
-      let currentIndex = dataObjects.indexOfObject(currentStock)
+    if let currentPageViewController = viewController as? SinglePageViewController, currentStock:StockQuoteItem = currentPageViewController.stock {
+      let currentIndex = dataController.indexOfStock(currentStock)
       return viewControllerAtIndex(currentIndex + 1)
     }
     return nil
   }
-  
-  func viewControllerAtIndex(index: Int) -> UIViewController? {
-    if (index < 0 || index > dataObjects.count - 1) {
-      return nil
-    }
-    let storyboard = UIStoryboard(name: "Main", bundle: nil)
-    let vc = storyboard.instantiateViewControllerWithIdentifier("SinglePageViewController") as! SinglePageViewController
-    let dataObject:AnyObject = dataObjects[index]
-    vc.stock = dataObject as? StockQuoteItem
-    return vc
-  }
-  
-}
 
+  func viewControllerAtIndex(index: Int) -> UIViewController? {
+    if let stock = dataController.stockAtIndex(index) {
+      let storyboard = UIStoryboard(name: "Main", bundle: nil)
+      let vc = storyboard.instantiateViewControllerWithIdentifier("SinglePageViewController") as! SinglePageViewController
+      vc.stock = stock
+      return vc
+    }
+    return nil
+  }
+}
