fd8af8f8d91c9e090a7035e75e61b34b5273d0d2
Christina Moulton <christina@teakmobile.com>
2015-10-11 12:35:18 -0300

    Added Persistence Manager

---
+++ PersistenceManager.swift
@@ -0,0 +1,32 @@
+//
+//  PersistenceManager.swift
+//  grokPageViewController
+//
+//  Created by Christina Moulton on 2015-10-11.
+//  Copyright © 2015 Teak Mobile Inc. All rights reserved.
+//
+
+import Foundation
+
+enum Path: String {
+  case StockQuotes = "StockQuotes"
+}
+
+class PersistenceManager {
+  class private func documentsDirectory() -> NSString {
+    let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+    let documentDirectory = paths[0] as String
+    return documentDirectory
+  }
+  
+  class func saveNSArray(arrayToSave: NSArray, path: Path) {
+    let file = documentsDirectory().stringByAppendingPathComponent(path.rawValue)
+    NSKeyedArchiver.archiveRootObject(arrayToSave, toFile: file)
+  }
+  
+  class func loadNSArray(path: Path) -> NSArray? {
+    let file = documentsDirectory().stringByAppendingPathComponent(path.rawValue)
+    let result = NSKeyedUnarchiver.unarchiveObjectWithFile(file)
+    return result as? NSArray
+  }
+}
