598e9a0613b69fd2e2f72d35265c8cbdd30137de
Christina Moulton <christina@teakmobile.com>
2015-10-11 12:35:48 -0300

    Use PersistenceManager to load and save stock quotes in data controller

--- StocksDataController.swift
+++ StocksDataController.swift
@@ -10,19 +10,27 @@ import Foundation
 import UIKit
 
 class StocksDataController {
-  var dataObjects = NSMutableArray() // NSMutableArray because Swift arrays don't have objectAtIndex
+  var dataObjects = NSArray() // NSMutableArray because Swift arrays don't have objectAtIndex
   
   func loadStockQuoteItems(completionHandler: (NSError?) -> Void) {
     let symbols = ["AAPL", "GOOG", "YHOO"]
     StockQuoteItem.getFeedItems(symbols, completionHandler:{ (result) in
       if let error = result.error {
+        // show the saved values
+        if let values = PersistenceManager.loadNSArray(.StockQuotes) {
+          self.dataObjects = values
+        }
         completionHandler(error)
         return
       }
       if let stocks = result.value {
-        for stock in stocks { // because we're getting back a Swift array but it's easier to do the PageController in an NSMutableArray
-          self.dataObjects.addObject(stock)
+        let mutableObjects = NSMutableArray()
+        for stock in stocks { // because we're getting back a Swift array but it's easier to do the PageController with an NSArray
+          mutableObjects.addObject(stock)
         }
+        self.dataObjects = mutableObjects
+        // save them to the device
+        PersistenceManager.saveNSArray(self.dataObjects, path: .StockQuotes)
       }
       // success
       completionHandler(nil)
